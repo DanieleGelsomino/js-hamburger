456ca34d25987bb2c0897c1f6729b81529648629
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-07 16:56:32 +0100

    add console.log

--- main.js
+++ main.js
@@ -2,18 +2,20 @@
 console.log('JS OK!')
 
 // seleziono l'icona dell'hamburger menu
-const dropDown = document.querySelector(".fa-bars")
+const dropDown = document.querySelector(".header-right > a")
 // aggiungo evento display block al click
 dropDown.addEventListener('click', function(){
+    console.log('dropDown click');
     let menuBlock = document.querySelector(".hamburger-menu");
     menuBlock.style.display='block';
 
 });
 
 // seleziono l'icona close
-const closeDropDown = document.querySelector('.fa-times');
+const closeDropDown = document.querySelector('.close');
 // aggiungo evento display block al click
 closeDropDown.addEventListener('click', function(){ 
+    console.log('closeDropDown click');
     let closeMenu = document.querySelector(".hamburger-menu");
     closeMenu.style.display='none';
 });
